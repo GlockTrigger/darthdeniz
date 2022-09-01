a8c0bd840883669aa0f143e68465d10f7f555d77
GlockTrigger <dardogand@gmail.com>
2022-09-01 15:29:26 +0300

    awf

--- login2.js
+++ login2.js
@@ -5,7 +5,7 @@ var password=document.getElementById("password").value;
     if(username=="sanay"&& password=="sanay123")
     {
         alert("login succesful");
-        window.location.replace("https://glocktrigger.github.io/darthdeniz/login.html?")   
+        window.location.href="https://glocktrigger.github.io/darthdeniz/login.html?"
 
     
 
@@ -13,7 +13,7 @@ var password=document.getElementById("password").value;
     else
     {
         alert("login failed");
-        window.location.replace("https://glocktrigger.github.io/darthdeniz/index.html?")
+        window.location.href="https://glocktrigger.github.io/darthdeniz/index.html?"
     }
 
 
