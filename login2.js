47a4e3df833d8ae749a74c2afffdfc32c081c686
GlockTrigger <dardogand@gmail.com>
2022-09-01 12:45:37 +0300

    simple code changes

--- login2.js
+++ login2.js
@@ -2,19 +2,19 @@ function validate()
 {
 var username=document.getElementById("username").value;
 var password=document.getElementById("password").value;
-if(username=="sanay"&& password=="sanay123")
-{
-    alert("login succesful");
-    
+    if(username=="sanay"&& password=="sanay123")
+    {
+        alert("login succesful");
+        window.location.replace("https://glocktrigger.github.io/darthdeniz/login.html");
 
     
 
-}
-else
-{
-    alert("login failed");
-    window.location.replace('https://glocktrigger.github.io/darthdeniz/');
-}
+    }
+    else
+    {
+        alert("login failed");
+        window.location.replace("https://glocktrigger.github.io/darthdeniz/");
+    }
 
 
 }
